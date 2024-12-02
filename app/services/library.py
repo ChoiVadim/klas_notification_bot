8d9cc59ffb854ac76b6aebc2f41b0bd221e5b0b1
Tsoi Vadim <tsoivadim97@gmail.com>
2024-12-03 01:52:49 +0900

    [Fix] Search book SSL verify

--- app/services/library.py
+++ app/services/library.py
@@ -1,11 +1,8 @@
 import logging
 import asyncio
 import aiohttp
-from io import BytesIO
-from datetime import datetime
-import os
+import ssl
 
-import qrcode
 from bs4 import BeautifulSoup
 
 
@@ -17,23 +14,35 @@ async def fetch_books(query: str):
         "y": "0",
         "q": query,
     }
+
     url = "https://kupis.kw.ac.kr/eds/brief/integrationResult"
 
     list_of_books = []
 
+    # Create a context that doesn't verify SSL
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            beautiful_soup = BeautifulSoup(await response.text(), "html.parser")
-            table_scroll_box = beautiful_soup.find(
-                "ul", {"class": "resultList resultDetail"}
-            )
-            if table_scroll_box:
-                for item in table_scroll_box.find_all("li"):
-                    title = item.find("dd", {"class": "title"}).find("a").text
-                    isInLibrary = item.find("div", {"class": "holding"}).text.strip()
-                    if isInLibrary:
-                        info = item.find_all("dd", {"class": "info"})[2].text
-                        list_of_books.append((title, info))
+        try:
+            async with session.get(url, params=params, ssl=ssl_context) as response:
+                beautiful_soup = BeautifulSoup(await response.text(), "html.parser")
+                table_scroll_box = beautiful_soup.find(
+                    "ul", {"class": "resultList resultDetail"}
+                )
+                if table_scroll_box:
+                    for item in table_scroll_box.find_all("li"):
+                        title = item.find("dd", {"class": "title"}).find("a").text
+                        isInLibrary = item.find(
+                            "div", {"class": "holding"}
+                        ).text.strip()
+                        if isInLibrary:
+                            info = item.find_all("dd", {"class": "info"})[2].text
+                            list_of_books.append((title, info))
+        except aiohttp.ClientError as e:
+            logging.error(f"Error fetching books: {e}")
+            return []
 
     return list_of_books
 
